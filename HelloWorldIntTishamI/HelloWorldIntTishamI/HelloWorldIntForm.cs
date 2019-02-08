670b29a531309fbc5336adec902930a53433548d
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-02-08 13:07:59 -0500

    Made HelloWorldInt better

--- HelloWorldIntTishamI/HelloWorldIntTishamI/HelloWorldIntForm.cs
+++ HelloWorldIntTishamI/HelloWorldIntTishamI/HelloWorldIntForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Tisham Islam
+ * Created on: 08/02/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #6 - Hello World international
+ * This program that displays different forms of "Hello World" in different languages using various radio buttons.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,36 @@ namespace HelloWorldIntTishamI
         public frmHelloWorldInt()
         {
             InitializeComponent();
+        }
+
+        private void radBangla_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "ওহে বিশ্ব";
+        }
+
+        private void radEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "Hello, World!";
+        }
+
+        private void radFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "Bonjour, tout le monde!";
+        }
+
+        private void radSpanish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "Hola, Mundo!";
+        }
+
+        private void radCanadian_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "Hockey hockey, ice rink!";
+        }
+
+        private void radLatin_CheckedChanged(object sender, EventArgs e)
+        {
+            lblHelloWorldInt.Text = "Salve, Orbis Terrarum";
         }
     }
 }
