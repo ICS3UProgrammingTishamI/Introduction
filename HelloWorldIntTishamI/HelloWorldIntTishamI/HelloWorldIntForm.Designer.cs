de1cc7a72444ac7c803b05466ca94474c55bbc08
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-02-11 12:23:45 -0500

    Added assignment two and PerAreaVol

--- HelloWorldIntTishamI/HelloWorldIntTishamI/HelloWorldIntForm.Designer.cs
+++ HelloWorldIntTishamI/HelloWorldIntTishamI/HelloWorldIntForm.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.lblHelloWorldInt = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.radEnglish = new System.Windows.Forms.RadioButton();
-            this.radSpanish = new System.Windows.Forms.RadioButton();
-            this.radBangla = new System.Windows.Forms.RadioButton();
-            this.radFrench = new System.Windows.Forms.RadioButton();
-            this.radLatin = new System.Windows.Forms.RadioButton();
+            this.grbLanguages = new System.Windows.Forms.GroupBox();
             this.radCanadian = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
+            this.radLatin = new System.Windows.Forms.RadioButton();
+            this.radFrench = new System.Windows.Forms.RadioButton();
+            this.radBangla = new System.Windows.Forms.RadioButton();
+            this.radSpanish = new System.Windows.Forms.RadioButton();
+            this.radEnglish = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.grbLanguages.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblHelloWorldInt
@@ -51,85 +51,20 @@
             this.lblHelloWorldInt.TabIndex = 0;
             this.lblHelloWorldInt.Text = "Hello World";
             // 
-            // groupBox1
+            // grbLanguages
             // 
-            this.groupBox1.Controls.Add(this.radCanadian);
-            this.groupBox1.Controls.Add(this.radLatin);
-            this.groupBox1.Controls.Add(this.radFrench);
-            this.groupBox1.Controls.Add(this.radBangla);
-            this.groupBox1.Controls.Add(this.radSpanish);
-            this.groupBox1.Controls.Add(this.radEnglish);
-            this.groupBox1.Location = new System.Drawing.Point(105, 172);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1049, 346);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            // 
-            // radEnglish
-            // 
-            this.radEnglish.AutoSize = true;
-            this.radEnglish.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radEnglish.Location = new System.Drawing.Point(35, 79);
-            this.radEnglish.Name = "radEnglish";
-            this.radEnglish.Size = new System.Drawing.Size(64, 20);
-            this.radEnglish.TabIndex = 0;
-            this.radEnglish.TabStop = true;
-            this.radEnglish.Text = "English";
-            this.radEnglish.UseVisualStyleBackColor = true;
-            this.radEnglish.CheckedChanged += new System.EventHandler(this.radEnglish_CheckedChanged);
-            // 
-            // radSpanish
-            // 
-            this.radSpanish.AutoSize = true;
-            this.radSpanish.Font = new System.Drawing.Font("Prestige Elite Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radSpanish.Location = new System.Drawing.Point(539, 217);
-            this.radSpanish.Name = "radSpanish";
-            this.radSpanish.Size = new System.Drawing.Size(166, 40);
-            this.radSpanish.TabIndex = 1;
-            this.radSpanish.TabStop = true;
-            this.radSpanish.Text = "Spanish";
-            this.radSpanish.UseVisualStyleBackColor = true;
-            this.radSpanish.CheckedChanged += new System.EventHandler(this.radSpanish_CheckedChanged);
-            // 
-            // radBangla
-            // 
-            this.radBangla.AutoSize = true;
-            this.radBangla.Font = new System.Drawing.Font("OCR A Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radBangla.Location = new System.Drawing.Point(333, 161);
-            this.radBangla.Name = "radBangla";
-            this.radBangla.Size = new System.Drawing.Size(111, 24);
-            this.radBangla.TabIndex = 2;
-            this.radBangla.TabStop = true;
-            this.radBangla.Text = "Bangla";
-            this.radBangla.UseVisualStyleBackColor = true;
-            this.radBangla.CheckedChanged += new System.EventHandler(this.radBangla_CheckedChanged);
-            // 
-            // radFrench
-            // 
-            this.radFrench.AutoSize = true;
-            this.radFrench.Font = new System.Drawing.Font("Proxy 3", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radFrench.Location = new System.Drawing.Point(118, 255);
-            this.radFrench.Name = "radFrench";
-            this.radFrench.Size = new System.Drawing.Size(99, 23);
-            this.radFrench.TabIndex = 3;
-            this.radFrench.TabStop = true;
-            this.radFrench.Text = "French";
-            this.radFrench.UseVisualStyleBackColor = true;
-            this.radFrench.CheckedChanged += new System.EventHandler(this.radFrench_CheckedChanged);
-            // 
-            // radLatin
-            // 
-            this.radLatin.AutoSize = true;
-            this.radLatin.Font = new System.Drawing.Font("GothicG", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radLatin.Location = new System.Drawing.Point(892, 268);
-            this.radLatin.Name = "radLatin";
-            this.radLatin.Size = new System.Drawing.Size(95, 35);
-            this.radLatin.TabIndex = 4;
-            this.radLatin.TabStop = true;
-            this.radLatin.Text = "Latin";
-            this.radLatin.UseVisualStyleBackColor = true;
-            this.radLatin.CheckedChanged += new System.EventHandler(this.radLatin_CheckedChanged);
+            this.grbLanguages.Controls.Add(this.radCanadian);
+            this.grbLanguages.Controls.Add(this.radLatin);
+            this.grbLanguages.Controls.Add(this.radFrench);
+            this.grbLanguages.Controls.Add(this.radBangla);
+            this.grbLanguages.Controls.Add(this.radSpanish);
+            this.grbLanguages.Controls.Add(this.radEnglish);
+            this.grbLanguages.Location = new System.Drawing.Point(105, 172);
+            this.grbLanguages.Name = "grbLanguages";
+            this.grbLanguages.Size = new System.Drawing.Size(1049, 346);
+            this.grbLanguages.TabIndex = 1;
+            this.grbLanguages.TabStop = false;
+            this.grbLanguages.Text = "Languages";
             // 
             // radCanadian
             // 
@@ -144,17 +79,82 @@
             this.radCanadian.UseVisualStyleBackColor = true;
             this.radCanadian.CheckedChanged += new System.EventHandler(this.radCanadian_CheckedChanged);
             // 
+            // radLatin
+            // 
+            this.radLatin.AutoSize = true;
+            this.radLatin.Font = new System.Drawing.Font("GothicG", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLatin.Location = new System.Drawing.Point(892, 268);
+            this.radLatin.Name = "radLatin";
+            this.radLatin.Size = new System.Drawing.Size(95, 35);
+            this.radLatin.TabIndex = 4;
+            this.radLatin.TabStop = true;
+            this.radLatin.Text = "Latin";
+            this.radLatin.UseVisualStyleBackColor = true;
+            this.radLatin.CheckedChanged += new System.EventHandler(this.radLatin_CheckedChanged);
+            // 
+            // radFrench
+            // 
+            this.radFrench.AutoSize = true;
+            this.radFrench.Font = new System.Drawing.Font("Proxy 3", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radFrench.Location = new System.Drawing.Point(118, 255);
+            this.radFrench.Name = "radFrench";
+            this.radFrench.Size = new System.Drawing.Size(99, 23);
+            this.radFrench.TabIndex = 3;
+            this.radFrench.TabStop = true;
+            this.radFrench.Text = "French";
+            this.radFrench.UseVisualStyleBackColor = true;
+            this.radFrench.CheckedChanged += new System.EventHandler(this.radFrench_CheckedChanged);
+            // 
+            // radBangla
+            // 
+            this.radBangla.AutoSize = true;
+            this.radBangla.Font = new System.Drawing.Font("OCR A Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radBangla.Location = new System.Drawing.Point(333, 161);
+            this.radBangla.Name = "radBangla";
+            this.radBangla.Size = new System.Drawing.Size(111, 24);
+            this.radBangla.TabIndex = 2;
+            this.radBangla.TabStop = true;
+            this.radBangla.Text = "Bangla";
+            this.radBangla.UseVisualStyleBackColor = true;
+            this.radBangla.CheckedChanged += new System.EventHandler(this.radBangla_CheckedChanged);
+            // 
+            // radSpanish
+            // 
+            this.radSpanish.AutoSize = true;
+            this.radSpanish.Font = new System.Drawing.Font("Prestige Elite Std", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radSpanish.Location = new System.Drawing.Point(539, 217);
+            this.radSpanish.Name = "radSpanish";
+            this.radSpanish.Size = new System.Drawing.Size(166, 40);
+            this.radSpanish.TabIndex = 1;
+            this.radSpanish.TabStop = true;
+            this.radSpanish.Text = "Spanish";
+            this.radSpanish.UseVisualStyleBackColor = true;
+            this.radSpanish.CheckedChanged += new System.EventHandler(this.radSpanish_CheckedChanged);
+            // 
+            // radEnglish
+            // 
+            this.radEnglish.AutoSize = true;
+            this.radEnglish.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radEnglish.Location = new System.Drawing.Point(35, 79);
+            this.radEnglish.Name = "radEnglish";
+            this.radEnglish.Size = new System.Drawing.Size(64, 20);
+            this.radEnglish.TabIndex = 0;
+            this.radEnglish.TabStop = true;
+            this.radEnglish.Text = "English";
+            this.radEnglish.UseVisualStyleBackColor = true;
+            this.radEnglish.CheckedChanged += new System.EventHandler(this.radEnglish_CheckedChanged);
+            // 
             // frmHelloWorldInt
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1531, 582);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grbLanguages);
             this.Controls.Add(this.lblHelloWorldInt);
             this.Name = "frmHelloWorldInt";
             this.Text = "Hello World International by Tisham Islam";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.grbLanguages.ResumeLayout(false);
+            this.grbLanguages.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -163,7 +163,7 @@
         #endregion
 
         private System.Windows.Forms.Label lblHelloWorldInt;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grbLanguages;
         private System.Windows.Forms.RadioButton radCanadian;
         private System.Windows.Forms.RadioButton radLatin;
         private System.Windows.Forms.RadioButton radFrench;
