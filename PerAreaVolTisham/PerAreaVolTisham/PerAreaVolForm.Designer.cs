eb3fe9a527e2133c8909b9327b35984217be0da4
Tisham Islam <45071113+sl1mt1sh@users.noreply.github.com>
2019-02-12 12:16:01 -0500

    Final Update?= in case there was anything unchanged

--- PerAreaVolTisham/PerAreaVolTisham/PerAreaVolForm.Designer.cs
+++ PerAreaVolTisham/PerAreaVolTisham/PerAreaVolForm.Designer.cs
@@ -28,26 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnSphere = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.lblRectangle = new System.Windows.Forms.Label();
+            this.lblCircle = new System.Windows.Forms.Label();
+            this.lblSphere = new System.Windows.Forms.Label();
+            this.lblSphereAnswer = new System.Windows.Forms.Label();
+            this.lblCircleAnswer = new System.Windows.Forms.Label();
+            this.lblRectangleAnswer = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.lblTest = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // btnRectangle
             // 
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(92, 29);
-            this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(1);
-            this.label1.Size = new System.Drawing.Size(348, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "The rectangle has a length of 8, and a width of 5. What is its perimeter?";
+            this.btnRectangle.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRectangle.Location = new System.Drawing.Point(336, 35);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(262, 50);
+            this.btnRectangle.TabIndex = 0;
+            this.btnRectangle.Text = "I\'m not sure, what is it?";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnSphere
+            // 
+            this.btnSphere.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSphere.Location = new System.Drawing.Point(336, 453);
+            this.btnSphere.Name = "btnSphere";
+            this.btnSphere.Size = new System.Drawing.Size(262, 50);
+            this.btnSphere.TabIndex = 1;
+            this.btnSphere.Text = "I\'m not sure, what is it?";
+            this.btnSphere.UseVisualStyleBackColor = true;
+            this.btnSphere.Click += new System.EventHandler(this.btnSphere_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCircle.Location = new System.Drawing.Point(336, 244);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(262, 50);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "I\'m not sure, what is it?";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // lblRectangle
+            // 
+            this.lblRectangle.AutoSize = true;
+            this.lblRectangle.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblRectangle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRectangle.Location = new System.Drawing.Point(104, 45);
+            this.lblRectangle.Name = "lblRectangle";
+            this.lblRectangle.Size = new System.Drawing.Size(216, 28);
+            this.lblRectangle.TabIndex = 3;
+            this.lblRectangle.Text = "What is the rectangle\'s perimeter? \r\nIt has a length of 12cm, and a width of 9cm." +
+    "\r\n";
+            // 
+            // lblCircle
+            // 
+            this.lblCircle.AutoSize = true;
+            this.lblCircle.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblCircle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCircle.Location = new System.Drawing.Point(104, 254);
+            this.lblCircle.Name = "lblCircle";
+            this.lblCircle.Size = new System.Drawing.Size(148, 28);
+            this.lblCircle.TabIndex = 4;
+            this.lblCircle.Text = "What\'s the area of the circle?\r\n It has a radius of 10m.";
+            // 
+            // lblSphere
+            // 
+            this.lblSphere.AutoSize = true;
+            this.lblSphere.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblSphere.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSphere.Location = new System.Drawing.Point(104, 453);
+            this.lblSphere.Name = "lblSphere";
+            this.lblSphere.Size = new System.Drawing.Size(168, 28);
+            this.lblSphere.TabIndex = 5;
+            this.lblSphere.Text = "What\'s the volume of the sphere?\r\n It has a radius of 42km.";
+            // 
+            // lblSphereAnswer
+            // 
+            this.lblSphereAnswer.AutoSize = true;
+            this.lblSphereAnswer.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblSphereAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSphereAnswer.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSphereAnswer.Location = new System.Drawing.Point(654, 464);
+            this.lblSphereAnswer.Name = "lblSphereAnswer";
+            this.lblSphereAnswer.Size = new System.Drawing.Size(86, 30);
+            this.lblSphereAnswer.TabIndex = 6;
+            this.lblSphereAnswer.Text = "????????";
+            // 
+            // lblCircleAnswer
+            // 
+            this.lblCircleAnswer.AutoSize = true;
+            this.lblCircleAnswer.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblCircleAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCircleAnswer.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircleAnswer.Location = new System.Drawing.Point(654, 254);
+            this.lblCircleAnswer.Name = "lblCircleAnswer";
+            this.lblCircleAnswer.Size = new System.Drawing.Size(86, 30);
+            this.lblCircleAnswer.TabIndex = 7;
+            this.lblCircleAnswer.Text = "????????";
+            // 
+            // lblRectangleAnswer
+            // 
+            this.lblRectangleAnswer.AutoSize = true;
+            this.lblRectangleAnswer.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblRectangleAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRectangleAnswer.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRectangleAnswer.Location = new System.Drawing.Point(654, 45);
+            this.lblRectangleAnswer.Name = "lblRectangleAnswer";
+            this.lblRectangleAnswer.Size = new System.Drawing.Size(86, 30);
+            this.lblRectangleAnswer.TabIndex = 8;
+            this.lblRectangleAnswer.Text = "????????";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.Location = new System.Drawing.Point(895, 118);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(262, 50);
+            this.btnTest.TabIndex = 9;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblTest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTest.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.Location = new System.Drawing.Point(1243, 129);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(86, 30);
+            this.lblTest.TabIndex = 10;
+            this.lblTest.Text = "????????";
             // 
             // frmPerAreaVol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1444, 620);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lblRectangleAnswer);
+            this.Controls.Add(this.lblCircleAnswer);
+            this.Controls.Add(this.lblSphereAnswer);
+            this.Controls.Add(this.lblSphere);
+            this.Controls.Add(this.lblCircle);
+            this.Controls.Add(this.lblRectangle);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnSphere);
+            this.Controls.Add(this.btnRectangle);
             this.Name = "frmPerAreaVol";
             this.Text = "Perimeter, Area Volume by Tisham Islam";
             this.ResumeLayout(false);
@@ -57,7 +192,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnSphere;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Label lblRectangle;
+        private System.Windows.Forms.Label lblCircle;
+        private System.Windows.Forms.Label lblSphere;
+        private System.Windows.Forms.Label lblSphereAnswer;
+        private System.Windows.Forms.Label lblCircleAnswer;
+        private System.Windows.Forms.Label lblRectangleAnswer;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label lblTest;
     }
 }
 
--- PerAreaVolTisham/PerAreaVolTisham/PerAreaVolForm.cs
+++ PerAreaVolTisham/PerAreaVolTisham/PerAreaVolForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Tisham Islam
+ * Created on: 11/02/2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - Perimeter, Area, Volume
+ * This program calculates the perimeter of a rectangle, the area of a circle and the volume of a sphere.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,26 @@ namespace PerAreaVolTisham
         public frmPerAreaVol()
         {
             InitializeComponent();
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            lblRectangleAnswer.Text = Convert.ToString((9 + 12) * 2) + "cm";
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            lblCircleAnswer.Text = Convert.ToString(Math.PI * Math.Pow(10,2)) + "m^2";
+        }
+
+        private void btnSphere_Click(object sender, EventArgs e)
+        {
+            lblSphereAnswer.Text = Convert.ToString(Math.PI * 4 / 3 * Math.Pow(42, 3)) + "km^3";
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblTest.Text = Convert.ToString(4.0 / 3.0);
         }
     }
 }
